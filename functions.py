fcbbf390ebbeac212eeb907a2c1a41a90d068d0e
pablo <pablochova02@gmail.com>
2023-03-22 10:18:08 +0100

    commit_1

---
+++ functions.py
@@ -0,0 +1,23 @@
+def read_data(fichero):
+
+
+    {
+    'dato':{'type':'white',
+            'fixed acidity':'7',
+            'volatile acidity': '0.27',
+            'citric acid':'0.36',
+            'residual sugar':'20.7',
+            'chlorides':'0.045',
+            'free sulfur dioxide':'45',
+            'total sulfur dioxide':'170',
+            'density':'1.001',
+            'PH':'3',
+            'sulphates':'0.45',
+            'alcohol':'8.8',
+            'quality':'6',
+        },
+        
+    }
+
+    }
+
